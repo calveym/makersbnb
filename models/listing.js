155dd7a2aa0fda33006ac66964cf15d43324a8c2
LMCMLJ <mjcalvey2@gmail.com>
2016-10-31 16:56:49 +0000

    Adds sequelize and sequelize-cli to package.json

--- models/listing.js
+++ models/listing.js
@@ -15,13 +15,16 @@ var Listing = sequelize.define('listing', {
     type: sequelize.DOUBLE,
     field: 'field'
   },
-
+  image: {
+    type: sequelize.STRING
+  }
 });
 
 Listing.sync({force:true}).then(function(){
   return Listing.create({
     name: 'Broadmoore Hospital',
     description: 'the place for Jack the Ripper',
-    price: 55
+    price: 55,
+    image: "http://www.slate.com/content/dam/slate/blogs/browbeat/2015/01/06/wesshining.jpg.CROP.promovar-mediumlarge.jpg"
   });
 });
