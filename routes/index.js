af67d9c2da91ca627509072b9382b9b4c9838c09
LMCMLJ <mjcalvey2@gmail.com>
2016-11-01 15:35:47 +0000

    Merging up to current orm

--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,23 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/user/create', function(req, res) {
-    models.User.create({
-    username: req.body.username,
-    email: req.body.email,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
-    res.redirect('/');
-});
-
-router.get('/new_listing', function(req, res) {
-  models.Listing.findAll({}).then(function(listings) {
-    res.json(listing);
-  });
-});
-
 router.post('/new_listing', function(req, res) {
-    models.Listing.create({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    image: req.body.image,
-  });
+  console.log("type:");
+  try {
+    console.log("1");
+    var listing = Listing.create({
+      name: 'flat',
+      description: '2 bedrooms',
+      price: 45,
+      image: 'jhjhjh'
+    });
+    console.log("This runs");
+  } catch (e) {
+    console.log(e);
+  }
 
+  res.redirect('/');
+  console.log("gasjgal");
 });
-
 
 module.exports = router;
